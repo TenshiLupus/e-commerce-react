83bc1ad8233127ba03cad154b284b83034faa92d
Tenshilupus <Angel__CM@hotmail.com>
2023-02-12 12:00:25 +0100

    Routes init

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
-import Directory from './components/Directory/Directory.component';
+import {Routes, Route} from 'react-router-dom'
+import Home from './components/Routes/Home/Home.component';
+
+//Routes need to be children of the routes component to access the subcomponents
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+    </div>
+  )
+}
+
+const Shop = () => {
+  return <h1>Im am the shop page</h1>
+}
 
 const App = () => {
-
-  const categories = [
-      {
-        "id": 1,
-        "title": "hats",
-        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-      },
-      {
-        "id": 2,
-        "title": "jackets",
-        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-      },
-      {
-        "id": 3,
-        "title": "sneakers",
-        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-      },
-      {
-        "id": 4,
-        "title": "womens",
-        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-      },
-      {
-        "id": 5,
-        "title": "mens",
-        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-      }
-  ];
-
   return (
-    <Directory categories={categories}/>
+    <Routes>
+      <Route path='/home' element={<Home/>}>
+        <Route path='shop' element={<Shop/>}/>
+      </Route>
+    </Routes>
   )
 }
 
