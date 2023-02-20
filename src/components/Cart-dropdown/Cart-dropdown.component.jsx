34b86c6cbcca178ed780a6fa978ff9a9d78bf55e
Tenshilupus <Angel__CM@hotmail.com>
2023-02-20 17:39:17 +0100

    cart item comments

--- src/components/Cart-dropdown/Cart-dropdown.component.jsx
+++ src/components/Cart-dropdown/Cart-dropdown.component.jsx
@@ -1,12 +1,19 @@
+import {useContext} from 'react';
+import {CartContext} from '../../Contexts/Cart.context';
+
 import Button from '../Button/Button.component';
+import CartItem from '../cart-item/CartItem.component';
 import './Cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
+
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
-            <Button>Go to checkout</Button>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
             </div>
+            <Button>Go to checkout</Button>
         </div>
     )
 }
