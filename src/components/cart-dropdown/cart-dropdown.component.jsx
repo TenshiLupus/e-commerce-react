20a0603ba83e238dcf15b17f57602ca653b078bd
Tenshilupus <Angel__CM@hotmail.com>
2023-03-01 15:52:18 +0100

    renaming folders

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,7 +5,7 @@ import {useNavigate} from 'react-router-dom';
 import {CartContext} from '../../Contexts/Cart.context';
 
 import Button from '../button/button.component';
-import CartItem from '../cart-ite/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import {CartDropDownContainer, EmptyMessage, CartItems} from './cart-dropdown.styles';
 
