2f618806282ff0dac0edda54d85e1f917d1d0896
Tenshilupus <Angel__CM@hotmail.com>
2023-03-01 14:18:42 +0100

    renaming

--- src/Routes/Home/Home.component.jsx
+++ src/Routes/Home/Home.component.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Directory from "../../Components/Directory/Directory.component";
+import Directory from "../../Components/directory/Directory.component";
 
 const Home = () => {
   
