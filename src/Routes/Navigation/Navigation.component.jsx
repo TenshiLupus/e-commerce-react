1449acd3c432baec5e6803af2902023ec4acb036
Tenshilupus <Angel__CM@hotmail.com>
2023-03-01 14:02:02 +0100

    refact

--- src/Routes/Navigation/Navigation.component.jsx
+++ src/Routes/Navigation/Navigation.component.jsx
@@ -1,10 +1,5 @@
 import {Fragment, useContext} from 'react';
-import { Outlet} from "react-router-dom";
-
-
-
-import CartDropdown from '../../Components/Cart-dropdown/Cart-dropdown.component';
-import CartIcon from '../../Components/Cart-icon/Cart-icon.component';
+import {Outlet} from "react-router-dom";
 
 import {ReactComponent as CrownLogo} from '../../Assets/crown.svg';
 
@@ -14,6 +9,9 @@ import { CartContext } from '../../Contexts/Cart.context';
 import {signOutUser} from '../../Utils/Firebase/Firebase.utils';
 
 import {NavigationContainer, NavLink, NavLinks, LogoContainer} from './Navigation.styles';
+
+import CartIcon from '../../Components/Cart-icon/Cart-icon.component';
+import CartDropdown from '../../Components/Cart-dropdown/Cart-dropdown.component'
 
 //base component functionality is being inherited from the styled-components inported in the styles file. Hence the ability to treat the styled components as normal components
 const Navigation = () => {
